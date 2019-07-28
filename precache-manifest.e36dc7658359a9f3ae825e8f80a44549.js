4eba66f2c6fb35f96cd3007e061cf08a33c3ee96
Ninh <ninhnguyen375@gmail.com>
2019-07-28 12:10:03 +0700

    hi

---
+++ precache-manifest.e36dc7658359a9f3ae825e8f80a44549.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f11bacb794d7ddefcdaac5012e03f395",
+    "url": "test-host-github/index.html"
+  },
+  {
+    "revision": "fa989121443cf8a56b19",
+    "url": "test-host-github/static/js/2.f1d152b3.chunk.js"
+  },
+  {
+    "revision": "1b7fd53a76dcabecf0e9",
+    "url": "test-host-github/static/js/main.d7d02147.chunk.js"
+  },
+  {
+    "revision": "7f0641272712adafc7b3",
+    "url": "test-host-github/static/js/runtime~main.1429cbc5.js"
+  }
+]);
